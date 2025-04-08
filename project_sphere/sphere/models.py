cd69fb374192fbbacb384d9c1f0efe393138007c
Дмитрий Федоров <fedorov_da@rmlab.stc-spb.ru>
2025-04-08 21:54:43 +0300

    add admin panel

--- project_sphere/sphere/models.py
+++ project_sphere/sphere/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(default = 'city', max_length = 50)
+    latitude = models.FloatField(default = 0)
+    longitude = models.FloatField(default=0)
 
--- project_sphere/sphere/views.py
+++ project_sphere/sphere/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpRequest
+from .models import City
 
 class Index(View):
     def get(self, request):
-        return render(request, 'sphere/index.html')
+        context = {
+            'title': 'sphere',
+            'cities': City.objects.all()
+        }
+        return render(request, 'sphere/index.html', context = context)
